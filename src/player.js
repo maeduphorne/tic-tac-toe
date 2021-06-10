fef04aca63d7102d7dbd71940f7484a4ef353757
Mae Duphorne <maelduphorne@gmail.com>
2021-06-10 15:51:20 -0600

    Add required methods to game and player classes

--- src/player.js
+++ src/player.js
@@ -4,4 +4,18 @@ class Player {
     this.token =
     this.wins = [];
   }
+
+  saveWinstoStorage() {
+    // stringify wins each time the variable is updated and set to storage
+  }
+
+  retrieveWinsFromStorage() {
+    //get wins from storage, parse to represents a JS object
+    //update number in data model (reassign this.wins to whatever number number is in localStorage)
+    //display on DOM
+  }
+  winAGame() {
+    // if this player wins a game,
+    // incremement number of wins by 1
+  }
 }
