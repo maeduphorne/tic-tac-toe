51a9ae67ad5b17842475b9eae5cbe8fd83f22529
Mae Duphorne <maelduphorne@gmail.com>
2021-06-13 13:33:40 -0600

    Display player tokens on click

--- src/game.js
+++ src/game.js
@@ -7,7 +7,7 @@ class Game {
     this.turn = this.players.player1;
     this.gameBoard = [null, null, null, null, null, null, null, null, null];
     this.possibleWins = [
-    [this.gameBoard[0], this.gameBoard[1], this.gameBoard[3]],
+    [this.gameBoard[0], this.gameBoard[1], this.gameBoard[2]],
     [this.gameBoard[3], this.gameBoard[4], this.gameBoard[5]],
     [this.gameBoard[6], this.gameBoard[7], this.gameBoard[8]],
     [this.gameBoard[2], this.gameBoard[4], this.gameBoard[6]],
@@ -25,6 +25,7 @@ class Game {
       };
     };
 
+//trackdata
     assignPlayerSpace(index) {
       if (this.turn === this.players.player1) {
         this.gameBoard[index] = 1;
@@ -33,11 +34,10 @@ class Game {
       }
     };
 
-
     checkWin() {
       for (var i = 0; i < this.possibleWins.length; i++) {
         if (this.possibleWins[i][0] === this.possibleWins[i][1] === this.possibleWins[i][2]) {
-          console.log(WIN!)
+          console.log("WIN!")
           // return true;
         }
         // else {
