8cb0f74823c1b392e840ce941108bf8cbdec2f80
Mae Duphorne <maelduphorne@gmail.com>
2021-06-15 12:42:12 -0600

    Move click event to individual boxes to fix icon replacement bug

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
-var board = document.getElementById('gameBoard');
 var heading = document.getElementById('header');
 var playerOneWins = document.getElementById('waterPlayerWins')
 var playerTwoWins = document.getElementById('firePlayerWins')
-var boardBoxes = document.querySelectorAll('game-board-box')
+var boardBoxes = document.querySelectorAll('.game-board-box')
 var currentGame = new Game();
 
-board.addEventListener('click', function(event){
-  playTurn(event);
-});
 window.addEventListener('load', loadPage);
 
 function loadPage() {
@@ -15,29 +11,21 @@ function loadPage() {
   currentGame.player2.retrieveWinsFromStorage();
   playerOneWins.innerText = `${currentGame.player1.wins} Wins`
   playerTwoWins.innerText = `${currentGame.player2.wins} Wins`
+  for (var i = 0; i < boardBoxes.length; i++) {
+    console.log(boardBoxes)
+    boardBoxes[i].addEventListener('click', function(event){
+      playTurn(event);
+    });
+  }
   };
 
 function playTurn(event) {
-  if (event.target.id === 'zero') {
-    currentGame.assignPlayerSpace(0);
-  } else if (event.target.id === 'one') {
-    currentGame.assignPlayerSpace(1);
-  } else if (event.target.id === 'two') {
-    currentGame.assignPlayerSpace(2);
-  } else if (event.target.id === 'three') {
-    currentGame.assignPlayerSpace(3);
-  } else if (event.target.id === 'four') {
-    currentGame.assignPlayerSpace(4);
-  } else if (event.target.id === 'five') {
-    currentGame.assignPlayerSpace(5);
-  } else if (event.target.id === 'six') {
-    currentGame.assignPlayerSpace(6);
-  } else if (event.target.id === 'seven') {
-    currentGame.assignPlayerSpace(7);
-  } else if (event.target.id === 'eight') {
-    currentGame.assignPlayerSpace(8);
-  };
-  //disableclickevent for icon?
+  console.log(boardBoxes);
+  for (var i = 0; i < boardBoxes.length; i++) {
+    if (event.target.id === boardBoxes[i].id){
+      currentGame.assignPlayerSpace(boardBoxes[i].id);
+    }
+  }
   currentGame.tallyPlays();
   displayPlayerIcon();
   currentGame.toggleTurn();
@@ -47,7 +35,6 @@ function playTurn(event) {
   currentGame.checkPlayerTwoWin();
   currentGame.player2.saveWinsToStorage();
   winGame();
-  // console.log(currentGame)
 };
 
 function winGame(){
@@ -66,15 +53,10 @@ function updateWinsDisplay() {
 };
 
 function displayPlayerIcon() {
-  // it's adding disable click to the entire gameboard
-    if (currentGame.turn === currentGame.player1 && event.target.id ===
-      'zero'||'one'||'two'||'three'||'four'||'five'||'six'||'seven'||'eight'){
-        console.log('player1')
+    if (currentGame.turn === currentGame.player1){
       event.target.innerText = '💧'
       event.target.classList.add('disable-click');
-    } else if (currentGame.turn === currentGame.player2 && event.target.id ===
-      'zero'||'one'||'two'||'three'||'four'||'five'||'six'||'seven'||'eight'){
-        console.log('player2')
+    } else if (currentGame.turn === currentGame.player2){
       event.target.innerText = '🔥'
       event.target.classList.add('disable-click');
     };
@@ -90,7 +72,9 @@ function updateTurnDisplay() {
 
 function disableBoard() {
   if (currentGame.won === true) {
-    board.classList.add('disable-click');
+    for (var i = 0; i < boardBoxes.length; i++) {
+    boardBoxes[i].classList.add('disable-click');
+  }
   };
 };
 
